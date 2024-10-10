7a0f8cddd1c2764010763f34ecb09696fc013ee8
eggacheb <115781643+eggacheb@users.noreply.github.com>
2024-10-11 00:09:42 +0800

    Update [...].ts

--- routes/[...].ts
+++ routes/[...].ts
@@ -1,17 +1,1 @@
 const PROXY_URL = 'http://128.204.223.70:17667/'
-
-export default defineEventHandler(async event => {
-    const url = getRequestURL(event)
-    const headers = getRequestHeaders(event)
-    const body = await readRawBody(event)
-    const res = await fetch(PROXY_URL + url.pathname + url.search, {
-        method: event.method,
-        headers: headers,
-        body,
-    })
-    return new Response(res.body, {
-        status: res.status,
-        statusText: res.statusText,
-        headers: res.headers,
-    })
-})
